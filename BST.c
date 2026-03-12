c21d370ea2d1f1e2df05bd1cd4acc2d9795719cd
caozhengyuan <415774106@qq.com>
2026-03-12 16:43:49 +0800

    Seventh

--- BST.c
+++ BST.c
@@ -21,13 +21,13 @@ Node* root = NULL;
 
 void Insert(int32_t data);
 Node* InsertR(Node* root, int32_t data);
-void Search(int32_t data);
+Node* Search(int32_t data);
 bool SearchR(Node* root, int32_t data);
-void Delete(int32_t data);
+Node* Delete(Node* root, int32_t data);
 int32_t Max(int32_t a, int32_t b);
 int32_t FindHeight(Node* root);
-int32_t FindMin();
-int32_t FindMax();
+Node* FindMin(Node* root);
+Node* FindMax(Node* root);
 void PreOrder(Node* root);
 void InOrder(Node* root);
 void PostOrder(Node* root);
@@ -37,13 +37,29 @@ void Dequeue();
 Node* Front();
 bool isFull();
 bool isEmpty();
+bool isBinarySearchTree(Node* root, int32_t min_value, int32_t max_value);
+Node* GetSuccessor(Node* root, int32_t data);
 
 int main() {
-    Insert(1);
+    Insert(9);
     Insert(2);
     Insert(3);
-    Search(2);
-    LevelOrder();
+    Insert(8);
+    Insert(16);
+    Insert(4);
+    Insert(12);
+    Insert(7);
+    root = Delete(root,4);
+    Node* successor = GetSuccessor(root, 7);
+    printf("%" PRId32 "\n", successor->data);
+    bool check = isBinarySearchTree(root, INT32_MIN, INT32_MAX);
+    if (check) {
+        printf("this is binary search tree!\n");
+    }
+    else {
+        printf("this is not binary search tree!\n");
+    }
+    InOrder(root);
     return 0;
 }
 
@@ -58,33 +74,33 @@ void Insert(int32_t data) {
         return;
     }
     while (1) {
-    if (data <= temp->data && temp->left != NULL) {
-        temp = temp->left;
-    }
-    if (data <= temp->data && temp->left == NULL) {
-        temp->left = temp1;
-        return;
-    }
-    if (data > temp->data && temp->right != NULL) {
-        temp = temp->right;
-    }
-    if (data > temp->data && temp->right == NULL) {
-        temp->right = temp1;
-        return;
-    }
+        if (data <= temp->data) {
+            if (temp->left == NULL) {
+                temp->left = temp1;
+                return;
+            }
+            temp = temp->left;
+        }
+        if (data > temp->data) {
+            if (temp->right == NULL) {
+                temp->right = temp1;
+                return;
+            }
+            temp = temp->right;
+        }
     }
 }
 
-void Search(int32_t data) {
+Node* Search(int32_t data) {
     Node* temp = root;
     if (root == NULL) {
         printf("There are no elements!\n");
-        return;
+        return NULL;
     }
     while (temp != NULL) {
     if (data == temp->data) {
         printf("Have!\n");
-        return;
+        return temp;
     }
     else if (data < temp->data) {
         temp = temp->left;
@@ -94,6 +110,7 @@ void Search(int32_t data) {
     }
     }
     printf("Don't Have!\n");
+    return NULL;
 }
 
 Node* InsertR(Node* root, int32_t data) {
@@ -138,27 +155,25 @@ int32_t FindHeight(Node* root) {
     int32_t height = Max(left_height, right_height) + 1;
     return height;
 }
-int32_t FindMin() {
+Node* FindMin(Node* root) {
     if (root == NULL) {
         printf("There are no elements!\n");
-        return -1;
+        return NULL;
     }
-    Node* temp = root;
-    while (temp->left != NULL) {
-        temp = temp->left;
+    while (root->left != NULL) {
+        root = root->left;
     }
-    return temp->data;
+    return root;
 }
-int32_t FindMax() {
+Node* FindMax(Node* root) {
     if (root == NULL) {
         printf("There are no elements!\n");
-        return -1;
+        return NULL;
     }
-    Node* temp = root;
-    while (temp->right != NULL) {
-        temp = temp->right;
+    while (root->right != NULL) {
+        root = root->right;
     }
-    return temp->data;
+    return root;
 }
 
 int32_t Max(int32_t a, int32_t b) {
@@ -259,5 +274,82 @@ Node* Front() {
     }
     return queue[front];
 }
-void Delete(int32_t data) {
+
+//min_value是负无穷 max_value是正无穷
+bool isBinarySearchTree(Node* root, int32_t min_value, int32_t max_value) {
+    if (root == NULL) {
+        return true;
+    }
+    if (root->data > min_value && root->data < max_value
+        && isBinarySearchTree(root->left, min_value, root->data) 
+        && isBinarySearchTree(root->right, root->data, max_value)){
+            return true;
+        }
+    else {
+        return false;
+    }
+}
+
+Node* Delete(Node* root, int32_t data) {
+    if (root == NULL) {
+        return NULL;
+    }
+    if (data < root->data) {
+        root->left = Delete(root->left, data);
+    }
+    else if (data > root->data) {
+        root->right = Delete(root->right, data);
+    }
+    //找到了
+    else {
+        if (root->left == NULL && root->right == NULL) {
+            free(root);
+            return NULL;
+        }
+        else if (root->left == NULL) {
+            Node* temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL) {
+            Node* temp = root->left;
+            free(root);
+            return temp;
+        }
+        else {
+            Node* temp = root->right;
+            while (temp->left != NULL) {
+                temp = temp->left;
+            }
+            root->data = temp->data;
+            root->right = Delete(root->right, temp->data);
+            return root;
+        }
+    }
+    return root;
+}
+
+Node* GetSuccessor(Node* root, int32_t data) {
+    Node* current = Search(data);
+    if (current == NULL) {
+        return NULL;
+    }
+    if (current->right != NULL) {
+        Node* successor = FindMin(current->right);
+        return successor;
+    }
+    if (current->right == NULL) {
+        Node* successor = NULL;
+        while (root != current) {
+            if (root->data > data) {
+                successor = root;
+                root = root->left;
+            }
+            else {
+                root = root->right;
+            }
+        }
+        return successor;
+        }
+        return NULL;
 }
